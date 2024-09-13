4ec384a53e5f8fe5bcbacbcfbf62df860e306500
Bruno Valle <bvs.valle@gmail.com>
2024-09-13 14:52:31 -0300

    resolve problema marcadas/desmarcadas

--- index.js
+++ index.js
@@ -26,15 +26,15 @@ const listarMetas = async () => {
         instructions: false,
     })
 
-    if(respostas.length == 0) {
-        console.log("Nenhuma meta selcionada!")
-        return
-    }
-
     metas.forEach((m) => {
         m.checked = false
     })
 
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selcionada!")
+        return
+    }
+    
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
             return m.value == resposta
